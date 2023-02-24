d7a534ff2401a683b4a02c1f820a9ff97ea67321
diniyuha <diniyuha@gmail.com>
2023-02-24 09:56:00 +0600

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@
 import { Header } from "./components/header/Header"
 import { Navigation } from "./components/Navigation/Navigation"
 import { Catalog } from "./components/Catalog/Catalog"
+import { Provider } from "react-redux"
+import { store } from "./store"
 
 export const App = () => {
     return (
-   <>
+   <Provider store={store}>
     <Header />
     <main>
       <Navigation/>
       <Catalog/>
     </main>
     <footer></footer>
-   </>
+   </Provider>
     )
 }
 
